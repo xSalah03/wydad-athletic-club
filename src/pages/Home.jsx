71f6f4fc3ce50504a9154871a9a27a450a917e8a
Salahoddine LOUKHMI <se.loukhmi@gmail.com>
2023-06-02 09:22:19 +0100

    Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,6 @@ import { getInformations } from '../redux/apis/wacApi'
 import { useDispatch } from 'react-redux'
 import coach from '../assets/Mbenza1.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCediSign } from '@fortawesome/fontawesome-svg-core';
-
 
 function Home() {
 
@@ -22,7 +20,7 @@ function Home() {
                 <h2 className='text-slate-100 font-semibold text-4xl pt-8'>Wydad Athletic Club</h2>
                 <h4 className='text-slate-100 font-semibold text-3xl pt-8'>Champoins Club</h4>
                 <span>
-                    <FontAwesomeIcon icon={fac} />
+                    <FontAwesomeIcon />
                 </span>
             </div>
             <div><img src={coach} alt="Wac coach" /></div>
